79d4d6b783eb7235222187a2635b45ce07756d43
tevaat <tevaat@gmail.com>
2014-03-14 17:41:49 -1000

    modifroute

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,19 @@ FirstApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
-      match 'users' => 'users#welcome', :as => 'user_root'
+  root 'users#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+
+  root :to => "home#index"
+
+devise_for :users
+      match 'users' => 'users#welcome', :as => 'user_root'
+
+end
+
+resources :users, :only => :show
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
