8c2fabb28c72bdfe9e7eb7e38316b174b6697bcb
tevaat <tevaat@gmail.com>
2014-03-17 15:11:26 -1000

    modifroute&ajoutview

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 FirstApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+    root 'welcome#index'
+  
   # You can have the root of your site routed with "root"
   #  root 'users#index'
-    root 'topics#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-resources :users, :only => :show
+#resources :users, :only => :show
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
@@ -56,4 +57,7 @@ resources :users, :only => :show
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  
+    
 end
